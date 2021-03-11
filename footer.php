64ed89f37f17c36e48e9262b47e57bbd83b98c05
yogakurniawan420 <yogakurniawan420@gmail.com>
2021-03-11 16:01:32 +0800

    commit pertama: project praktikum Yoga Adhitya Kurniawan - 17630253

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div>
+
+<footer>
+    <div class="container">
+        <span>&copy; 2021 - FTI UNISKA</span>
+    </div>
+</footer>
+
+<script src="js/jquery.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ profile.php
@@ -0,0 +1,47 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/yoga.jpg" alt="Yoga Adhitya Kurniawan" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">17630253</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Yoga Adhitya Kurniawan</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Tabalong, 26 November 1998</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0878 1838 4974</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">yogakurniawan420@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/Yoga Adhitya Kurniawan" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/yogakrwn_" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
